101535aa30f258aa7814a4801baca887f06be1b1
Guilherme220501 <guilherme8876sk@gmail.com>
2024-10-22 15:11:38 -0300

    njnjn

--- formulario.php
+++ formulario.php
@@ -9,9 +9,11 @@ if (isset($_POST['submit']))
     $sexo = $_POST['genero'];
     $data_nasc = $_POST['data_nascimento'];
     $cpf = $_POST['cpf'];
+    $rg = $_POST['rg'];
     $estado = $_POST['estado'];
     $endereco = $_POST['endereco'];
 
-    $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,email,telefone,sexo,data_nasc,cpf,estado,endereco) VALUES ('$nome','$email','$telefone','$sexo','$data_nasc','$cpf','$estado','$endereco')");
+    $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,email,telefone,sexo,data_nasc,cpf,rg,estado,endereco) VALUES ('$nome','$email','$telefone','$sexo','$data_nasc','$cpf','$rg','$estado','$endereco')");
 }
+$mensagem = "Cadastro efetuado com sucesso!";
 ?>
